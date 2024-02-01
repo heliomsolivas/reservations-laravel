f6ca5e40594e778cb328e7290449dad520938e6d
Hélio M S Olivas <heliomsolivas@gmail.com>
2024-02-01 12:23:04 -0300

    Finishing CRUD companies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Company;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -13,5 +14,35 @@ class CompanyController extends Controller
         $companies = Company::all();
 
         return view("companies.index", compact("companies"));
+    }
+
+    public function create(): View
+    {
+        return view("companies.create");
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        Company::create($request->validated());
+
+        return to_route("companies.index");
+    }
+
+    public function edit(Company $company)
+    {
+        return view("companies.edit", compact("company"));
+    }
+
+    public function update(Request $request, Company $company): RedirectResponse
+    {
+        $company->update($request->validated());
+
+        return to_route("companies.index");
+    }
+
+    public function destroyu(Company $company): RedirectResponse
+    {
+        $company->delete();
+        return to_route("companies.index");
     }
 }
